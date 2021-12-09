4d83dc652d0a760438f6c88dce09eb379faf4710
DN_MinhHT <minhht@mcrew-tech.com>
2021-12-09 23:06:28 +0700

    Push source

---
+++ api/controllers/ArticlesController.js
@@ -0,0 +1,70 @@
+/**
+ * ArticlesController
+ *
+ * @description :: Server-side logic for managing articles
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+    list:function(req, res){
+        Articles.find({}).exec(function(err,articles){
+            if(err){
+                res.send(500, {error: 'Database Error'});
+            }
+            res.view('articles/list',{articles:articles});
+        })
+    },
+
+    add:function(req, res){
+        res.view('articles/add')
+    },
+
+    create:function(req, res){
+        var title = req.body.title;
+        var body = req.body.body;
+
+        Articles.create({title:title, body:body}).exec(function(err){
+            if(err){
+                res.send(500, {error: 'Database Error'});
+            }
+            res.redirect('/articles');
+        })
+    },
+
+    delete:function(req,res){
+        Articles.destroy({id:req.params.id}).exec(function(err){
+            if(err){
+                res.send(500, {error: 'Database Error'});
+            }
+            res.redirect('/articles');
+
+        })
+        return false;
+    },
+
+    edit:function(req, res){
+        Articles.findOne({id:req.params.id}).exec(function(err, article){
+
+            if (err){
+                res.send(500, {error: 'Database Error'});
+            }
+            res.view('articles/edit/',{article: article});
+
+        })
+    },
+
+    update:function(req, res){
+        var title = req.body.title;
+        var body = req.body.body;
+
+        Articles.update({id:req.params.id},{title:title, body:body}).exec(function(err){
+            if(err){
+                res.send(500, {error: 'Database Error'});
+            }
+            res.redirect('/articles');
+        })
+        return false;
+    }
+
+};
